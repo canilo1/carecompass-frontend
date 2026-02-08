0bb71a2ebf54385479e9bde52e5c07703d2be988
John Gutierrez <75353216+canilo1@users.noreply.github.com>
2026-02-07 16:20:12 -0800

    Update SignUpForm.jsx

--- src/components/login/SignUpForm.jsx
+++ src/components/login/SignUpForm.jsx
@@ -1,20 +1,29 @@
 import { useState, useContext } from 'react';
 import { useNavigate } from 'react-router';
+import { signUp } from '../../services/authService';
+import { UserContext } from '../../contexts/UserContext';
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from '../ui/card';
+import { Label } from '@radix-ui/react-label';
+import { Input } from "@/components/ui/input";
+import { Button } from '../ui/button';
 
-import { signUp } from '@/services/authService';
-
-import { UserContext } from '@/contexts/UserContext';
 const SignUpForm = () => {
   const navigate = useNavigate();
   const { setUser } = useContext(UserContext);
-  const [message, setMessage] = useState('');
   const [formData, setFormData] = useState({
     username: '',
+    email: '',
     password: '',
     passwordConf: '',
   });
-
-  const { username, password, passwordConf } = formData;
+  const [message, setMessage] = useState('');
 
   const handleChange = (evt) => {
     setMessage('');
@@ -23,6 +32,10 @@ const SignUpForm = () => {
 
   const handleSubmit = async (evt) => {
     evt.preventDefault();
+    if (formData.password !== formData.passwordConf) {
+      setMessage("Passwords do not match!");
+      return;
+    }
     try {
       const newUser = await signUp(formData);
       setUser(newUser);
@@ -32,54 +45,80 @@ const SignUpForm = () => {
     }
   };
 
-  const isFormInvalid = () => {
-    return !(username && password && password === passwordConf);
-  };
-
   return (
-    <main>
-      <h1>Sign Up</h1>
-      <p>{message}</p>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor='username'>Username:</label>
-          <input
-            type='text'
-            id='name'
-            value={username}
-            name='username'
-            onChange={handleChange}
-            required
-          />
+     <div className="min-h-screen flex items-center justify-center ">
+    <Card className="w-full max-w-sm rounded-sm ">
+      <CardHeader className="flex border-b-4 border-black-100 w-full font-[Poppins]">Care Compass</CardHeader>
+      <CardHeader>
+        <CardTitle class = "flex justify-center">Create Your Free Account</CardTitle>
+        <CardDescription>
+          Access Clinics, medical translations, and health resources in your language
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-col gap-6">
+            <div>
+              <Label htmlFor="username">Name (Optional)</Label>
+              <Input
+                id="username"
+                name="username"
+                placeholder="JohnSmith"
+                onChange={handleChange}
+                value={formData.username}
+              />
+            </div>
+            <div>
+              <Label htmlFor="email">Email *</Label>
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+                onChange={handleChange}
+                value={formData.email}
+              />
+            </div>
+            <div>
+              <Label htmlFor="password">Password *</Label>
+              <Input
+                id="password"
+                name="password"
+                type="password"
+                required
+                onChange={handleChange}
+                value={formData.password}
+              />
+            </div>
+            <div>
+              <Label htmlFor="passwordConf">Confirm Password *</Label>
+              <Input
+                id="passwordConf"
+                name="passwordConf"
+                type="password"
+                required
+                onChange={handleChange}
+                value={formData.passwordConf}
+              />
+            </div>
+          </div>
+          {message && <p className="text-red-500">{message}</p>}
+        </form>
+      </CardContent>
+      <CardFooter className="flex flex-col gap-2">
+        <div className="flex gap-3">
+          <Button variant="outline" className="w-full">Sign up with Google</Button>
+          <Button variant="outline" className="w-full">Sign up with Facebook</Button>
         </div>
-        <div>
-          <label htmlFor='password'>Password:</label>
-          <input
-            type='password'
-            id='password'
-            value={password}
-            name='password'
-            onChange={handleChange}
-            required
-          />
+        <div className="flex gap-3">
+          <Button className="bg-[#246ECB]" onClick={handleSubmit}>Sign up</Button>
+          <span>Or</span>
+          <Button className="bg-[#246ECB]" onClick={() => navigate("/sign-in")}>Sign In</Button>
         </div>
-        <div>
-          <label htmlFor='confirm'>Confirm Password:</label>
-          <input
-            type='password'
-            id='confirm'
-            value={passwordConf}
-            name='passwordConf'
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <button disabled={isFormInvalid()}>Sign Up</button>
-          <button onClick={() => navigate('/')}>Cancel</button>
-        </div>
-      </form>
-    </main>
+      </CardFooter>
+    </Card>
+    </div>
   );
 };
 
