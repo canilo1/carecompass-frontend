fae02b7c4290bbb99fedf27ceacfca20ecefdbac
John Gutierrez <75353216+canilo1@users.noreply.github.com>
2026-02-07 16:12:59 -0800

    Update SignInForm.jsx

--- src/components/login/SignInForm.jsx
+++ src/components/login/SignInForm.jsx
@@ -1,17 +1,24 @@
 import { useState, useContext } from 'react';
 import { useNavigate } from 'react-router';
-
-import { signIn } from '@/services/authService';
+import { signIn } from '../../services/authService';
 import { UserContext } from '../../contexts/UserContext';
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from '../ui/card';
+import { Label } from '@radix-ui/react-label';
+import { Input } from "@/components/ui/input";
+import { Button } from '../ui/button';
 
 const SignInForm = () => {
   const navigate = useNavigate();
   const { setUser } = useContext(UserContext);
+  const [formData, setFormData] = useState({ username: '', password: '' });
   const [message, setMessage] = useState('');
-  const [formData, setFormData] = useState({
-    username: '',
-    password: '',
-  });
 
   const handleChange = (evt) => {
     setMessage('');
@@ -30,40 +37,57 @@ const SignInForm = () => {
   };
 
   return (
-    <main>
-      <h1>Sign In</h1>
-      <p>{message}</p>
-      <form autoComplete='off' onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor='email'>Username:</label>
-          <input
-            type='text'
-            autoComplete='off'
-            id='username'
-            value={formData.username}
-            name='username'
-            onChange={handleChange}
-            required
-          />
+    <div className="min-h-screen flex items-center justify-center ">
+    <Card className="w-full max-w-sm rounded-sm">
+      <CardHeader className="flex border-b-4 border-black-100 w-full font-[Poppins]">Care Compass</CardHeader>
+      <CardHeader>
+        <CardTitle className = "flex justify-center">Sign In</CardTitle>
+        <CardDescription>
+          Access Clinics, medical translations, and health resources in your language
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-col gap-6">
+            <div>
+             <Label htmlFor="username">Username *</Label>
+<Input
+  id="username"
+  name="username"
+  placeholder="Username"
+  required
+  onChange={handleChange}
+  value={formData.username}
+/>
+            </div>
+            <div>
+              <Label htmlFor="password">Password *</Label>
+              <Input
+                id="password"
+                name="password"
+                type="password"
+                required
+                onChange={handleChange}
+                value={formData.password}
+              />
+            </div>
+          </div>
+          {message && <p className="text-red-500">{message}</p>}
+        </form>
+      </CardContent>
+      <CardFooter className="flex flex-col gap-2">
+        <div className="flex gap-3">
+          <Button variant="outline" className="w-full">Sign in with Google</Button>
+          <Button variant="outline" className="w-full">Sign in with Facebook</Button>
         </div>
-        <div>
-          <label htmlFor='password'>Password:</label>
-          <input
-            type='password'
-            autoComplete='off'
-            id='password'
-            value={formData.password}
-            name='password'
-            onChange={handleChange}
-            required
-          />
+        <div className="flex gap-3">
+          <Button className="bg-[#246ECB]" onClick={handleSubmit}>Sign In</Button>
+          <span>Or</span>
+          <Button className="bg-[#246ECB]" onClick={() => navigate("/sign-up")}>Create Account</Button>
         </div>
-        <div>
-          <button>Sign In</button>
-          <button onClick={() => navigate('/')}>Cancel</button>
-        </div>
-      </form>
-    </main>
+      </CardFooter>
+    </Card>
+    </div>
   );
 };
 
